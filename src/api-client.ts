835f59c8552722d2f3998af0c7ce15953d7e5626
Marek Vorp <marek.vorp@gmail.com>
2024-12-15 20:26:37 +0200

    homework 14

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,4 +1,5 @@
 import {APIRequestContext} from 'playwright'
+import {StatusCodes} from "http-status-codes";
 
 let baseURL: string = 'http://localhost:3000/users';
 
@@ -18,10 +19,34 @@ export class ApiClient {
         return ApiClient.instance
     }
 
+    async getUserById(userId: number): Promise<any | null> {
+        const response = await this.request.get(`${baseURL}/${userId}`);
+        if (response.status() === StatusCodes.NOT_FOUND) {
+            return null;
+        }
+        return response.json()
+    }
+
+    async getAllUsers(): Promise<any[]> {
+        const response = await this.request.get(baseURL);
+        return response.json()
+    }
+
     async createUsers(users: number): Promise<number> {
         for (let i = 0; i < users; i++) {
             const response = await this.request.post(baseURL)
         }
         return users
+    }
+
+    async deleteUserById(userId: number): Promise<void> {
+        const response = await this.request.delete(`${baseURL}/${userId}`);
+    }
+
+    async deleteAllUsers(): Promise<void> {
+        const users = await this.getAllUsers();
+        for (const user of users) {
+            await this.deleteUserById(user.id);
+        }
     }
 }
